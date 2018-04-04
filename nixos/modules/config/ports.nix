0b7dfe192fde44a240e164a57cd2dd88fa087554
Silvan Mosberger <infinisil@icloud.com>
2018-04-04 18:03:27 +0200

    ports: add step-by-step visualization

--- nixos/modules/config/ports.nix
+++ nixos/modules/config/ports.nix
@@ -36,16 +36,40 @@ in
     assertions = let
 
       attrs = config.networking.tcpServicePorts;
+      /* {
+        bar = [
+          { addr = "192.168.1.1"; port = 80; }
+        ];
+        foo = [
+          { addr = "any"; port = 80; }
+          { addr = "127.0.0.1"; port = 8080; }
+        ];
+      } */
 
       list = builtins.concatLists
         (map (n: map (a: a // { name = n; }) attrs.${n})
         (builtins.attrNames attrs));
+      /* [
+        { addr = "192.168.1.1"; name = "bar"; port = 80; }
+        { addr = "any"; name = "foo"; port = 80; }
+        { addr = "127.0.0.1"; name = "foo"; port = 8080; }
+      ] */
 
       perPort = foldl (acc: elem@{ port, ... }:
         acc // {
           "${toString port}" = (acc."${toString port}" or []) ++ [ elem ];
         }
       ) {} list;
+
+      /* {
+        "80" = [
+          { addr = "192.168.1.1"; name = "bar"; port = 80; }
+          { addr = "any"; name = "foo"; port = 80; }
+        ];
+        "8080" = [
+          { addr = "127.0.0.1"; name = "foo"; port = 8080; }
+        ];
+      } */
 
       final = mapAttrs (p:
         foldl (acc: { addr, name, ... }:
@@ -54,6 +78,23 @@ in
           }
         ) {}) perPort;
 
+      /* {
+        "80" = {
+          "192.168.1.1" = [ "bar" ];
+          any = [ "foo" ];
+        };
+        "8080" = {
+          "127.0.0.1" = [ "foo" ];
+        };
+      } */
+
+
+      /*
+        Checks a value such as {
+          "192.168.1.1" = [ "bar" ];
+          any = [ "foo" ];
+        } for having both bindings for any and other addresses, which is not valid
+      */
       checkAny = a: if a ? any && builtins.attrNames a != [ "any" ]
         then ''
           has both bindings for any and addresses ${
@@ -61,6 +102,7 @@ in
           }
         '' else null;
 
+      # Checks that all ports and addresses have exactly 1 service and checkAny
       valid = mapAttrs (p: a:
         builtins.attrValues (mapAttrs (addr: s:
           if length s != 1
@@ -68,6 +110,10 @@ in
           else null) a
         ) ++ [ (checkAny a) ]
       ) final;
+      /* {
+        "80" = [ null null "has both bindings for any and addresses 192.168.1.1" ];
+        "8080" = [ null null ];
+      } */
 
       problems = filter
         (x: ! isNull x)
@@ -78,6 +124,7 @@ in
               ${concatStringsSep "\n  " (filter (x: ! isNull x) v)}
           '' else null
         ) valid));
+      /* [ "Port 80 problems:\n\thas both bindings for any and addresses 192.168.1.1" ] */
     in [{
       assertion = problems == [];
       message = concatStringsSep "\n" problems;
