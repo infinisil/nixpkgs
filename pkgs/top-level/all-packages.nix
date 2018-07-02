infinisil/nixpkgs

commit 1fa2503f9dba814eb23726a25642d2180ce791c3

foundationdb: add 5.2.5 release, and new 6.0.0 snapshot
This requires a bit of fiddling with the ldflags patches and reworking a few
things about how the SCM info is configured. Ideally, not much more will change
before the 6.0 release, I think...

This also upgrades all FoundationDB packages to use the ordinary libressl
expression (which is now at 2.7.x), and changes around a few other things,
which will require a rebuild.

Signed-off-by: Austin Seipp <aseipp@pobox.com>

pkgs/top-level/all-packages.nix
@@ -2522,7 +2522,9 @@ with pkgs;
 
   fuseiso = callPackage ../tools/filesystems/fuseiso { };
 
-  fdbPackages = callPackage ../servers/foundationdb { stdenv = overrideCC stdenv gcc49; };
+  fdbPackages = callPackage ../servers/foundationdb {
+    stdenv49 = overrideCC stdenv gcc49;
+  };
 
   inherit (fdbPackages)
     foundationdb51

pkgs/servers/foundationdb/default.nix
@@ -1,17 +1,42 @@
-{ stdenv, fetchurl, fetchFromGitHub
+{ stdenv49
+, lib, fetchurl, fetchFromGitHub
+
 , which, findutils, m4, gawk
-, python, openjdk, mono58, libressl_2_6
-, boost16x
+, python, openjdk, mono58, libressl
 }:
 
 let
+  # hysterical raisins dictate a version of boost this old. however,
+  # we luckily do not need to build anything, we just need the header
+  # files.
+  boost152 = stdenv49.mkDerivation rec {
+    name = "boost-headers-1.52.0";
+
+    src = fetchurl {
+      url = "mirror://sourceforge/boost/boost_1_52_0.tar.bz2";
+      sha256 = "14mc7gsnnahdjaxbbslzk79rc0d12h1i681cd3srdwr3fzynlar2";
+    };
+
+    configurePhase = ":";
+    buildPhase = ":";
+    installPhase = "mkdir -p $out/include && cp -R boost $out/include/";
+  };
+
   makeFdb =
     { version
     , branch
-    , rev, sha256
+    , sha256
 
-    # fdb 6.0+ support boost 1.6x+, so default to it
-    , boost ? boost16x
+    # the revision can be inferred from the fdb tagging policy
+    , rev    ? "refs/tags/${version}"
+
+    # in theory newer versions of fdb support newer compilers, but they
+    # don't :( maybe one day
+    , stdenv ? stdenv49
+
+    # in theory newer versions of fdb support newer boost versions, but they
+    # don't :( maybe one day
+    , boost ? boost152
     }: stdenv.mkDerivation rec {
         name = "foundationdb-${version}";
         inherit version;
@@ -23,14 +48,20 @@ let
         };
 
         nativeBuildInputs = [ gawk which m4 findutils mono58 ];
-        buildInputs = [ python openjdk libressl_2_6 boost ];
+        buildInputs = [ python openjdk libressl boost ];
 
         patches =
-          [ ./fix-scm-version.patch
-            ./ldflags.patch
-          ];
+          [ # For 5.2+, we need a slightly adjusted patch to fix all the ldflags
+            (if lib.versionAtLeast version "5.2"
+             then ./ldflags.patch
+             else ./ldflags-5.1.patch)
+          ] ++
+          # for 6.0+, we do NOT need to apply this version fix, since we can specify
+          # it ourselves. see configurePhase
+          (lib.optional (!lib.versionAtLeast version "6.0") ./fix-scm-version.patch);
 
         postPatch = ''
+          # note: this does not do anything for 6.0+
           substituteInPlace ./build/scver.mk \
             --subst-var-by NIXOS_FDB_VERSION_ID "${rev}" \
             --subst-var-by NIXOS_FDB_SCBRANCH   "${branch}"
@@ -51,7 +82,13 @@ let
         enableParallelBuilding = true;
         makeFlags = [ "all" "fdb_c" "fdb_java" "KVRELEASE=1" ];
 
-        configurePhase = ":";
+        # on 6.0 and later, we can specify all this information manually
+        configurePhase = lib.optionalString (lib.versionAtLeast version "6.0") ''
+          export SOURCE_CONTROL=GIT
+          export SCBRANCH="${branch}"
+          export VERSION_ID="${rev}"
+        '';
+
         installPhase = ''
           mkdir -vp $out/{bin,libexec/plugins} $lib/{lib,share/java} $dev/include/foundationdb
 
@@ -85,45 +122,24 @@ let
        };
     };
 
-  # hysterical raisins dictate a version of boost this old. however,
-  # we luckily do not need to build anything, we just need the header
-  # files.
-  boost152 = stdenv.mkDerivation rec {
-    name = "boost-headers-1.52.0";
-
-    src = fetchurl {
-      url = "mirror://sourceforge/boost/boost_1_52_0.tar.bz2";
-      sha256 = "14mc7gsnnahdjaxbbslzk79rc0d12h1i681cd3srdwr3fzynlar2";
-    };
-
-    configurePhase = ":";
-    buildPhase = ":";
-    installPhase = "mkdir -p $out/include && cp -R boost $out/include/";
-  };
-
 in with builtins; {
 
-  foundationdb51 = makeFdb {
+  foundationdb51 = makeFdb rec {
     version = "5.1.7";
     branch  = "release-5.1";
-    rev     = "9ad8d02386d4a6a5efecf898df80f2747695c627";
     sha256  = "1rc472ih24f9s5g3xmnlp3v62w206ny0pvvw02bzpix2sdrpbp06";
-    boost   = boost152;
   };
 
   foundationdb52 = makeFdb rec {
-    version = "5.2.0pre1488_${substring 0 8 rev}";
-    branch  = "master";
-    rev     = "18f345487ed8d90a5c170d813349fa625cf05b4e";
-    sha256  = "0mz30fxj6q99cvjzg39s5zm992i6h2l2cb70lc58bdhsz92dz3vc";
-    boost   = boost152;
+    version = "5.2.5";
+    branch  = "release-5.2";
+    sha256  = "00csr4v9cwl9y8r63p73grc6cvhlqmzcniwrf80i0klxv5asg7q7";
   };
 
   foundationdb60 = makeFdb rec {
-    version = "6.0.0pre1636_${substring 0 8 rev}";
+    version = "6.0.0pre2227_${substring 0 8 rev}";
     branch  = "master";
-    rev     = "1265a7b6d5e632dd562b3012e70f0727979806bd";
-    sha256  = "0z1i5bkbszsbn8cc48rlhr29m54n2s0gq3dln0n7f97gf58mi5yf";
+    rev     = "8caa6eaecf1eeec0298fc77db334761b0c1d1523";
+    sha256  = "1q200rpsphl5fzwzp2vk7ifgsnqh95k0xfiicfi1c8253ylnsgll";
   };
-
 }

pkgs/servers/foundationdb/ldflags-5.1.patch
@@ -1,9 +1,9 @@
 diff --git a/FDBLibTLS/local.mk b/FDBLibTLS/local.mk
-index 5e6b9cfb..73f4e5f3 100644
+index 0b6eac8..b1891ca 100644
 --- a/FDBLibTLS/local.mk
 +++ b/FDBLibTLS/local.mk
 @@ -1,6 +1,5 @@
- FDBLibTLS_CFLAGS := -fPIC -I/usr/local/include -I$(BOOSTDIR) -Ifdbrpc
+ FDBLibTLS_CFLAGS := -fPIC -I/usr/local/include -I$(BOOSTDIR)
 -FDBLibTLS_STATIC_LIBS := -ltls -lssl -lcrypto
 -FDBLibTLS_LDFLAGS := -L/usr/local/lib -static-libstdc++ -static-libgcc -lrt
 +FDBLibTLS_LDFLAGS := -L/usr/local/lib -static-libstdc++ -static-libgcc -lrt -ltls -lssl -lcrypto
@@ -11,7 +11,7 @@ index 5e6b9cfb..73f4e5f3 100644
  
  # The plugin isn't a typical library, so it feels more sensible to have a copy
 diff --git a/bindings/c/local.mk b/bindings/c/local.mk
-index 44f0c31b..7aea5a4f 100644
+index 44f0c31..7aea5a4 100644
 --- a/bindings/c/local.mk
 +++ b/bindings/c/local.mk
 @@ -29,8 +29,8 @@ fdb_c_tests_HEADERS := -Ibindings/c
@@ -26,7 +26,7 @@ index 44f0c31b..7aea5a4f 100644
  endif
  
 diff --git a/bindings/flow/tester/local.mk b/bindings/flow/tester/local.mk
-index 2ef4fcb7..6e59625c 100644
+index 2ef4fcb..6e59625 100644
 --- a/bindings/flow/tester/local.mk
 +++ b/bindings/flow/tester/local.mk
 @@ -35,8 +35,7 @@ _fdb_flow_tester_clean:
@@ -40,7 +40,7 @@ index 2ef4fcb7..6e59625c 100644
    fdb_flow_tester_LDFLAGS += -lc++
  endif
 diff --git a/fdbbackup/local.mk b/fdbbackup/local.mk
-index 033fe7d4..865fc923 100644
+index 033fe7d..865fc92 100644
 --- a/fdbbackup/local.mk
 +++ b/fdbbackup/local.mk
 @@ -25,8 +25,7 @@ fdbbackup_LDFLAGS := $(fdbrpc_LDFLAGS)
@@ -54,7 +54,7 @@ index 033fe7d4..865fc923 100644
    # GPerfTools profiler (uncomment to use)
    # fdbbackup_CFLAGS += -I/opt/gperftools/include -DUSE_GPERFTOOLS=1
 diff --git a/fdbcli/local.mk b/fdbcli/local.mk
-index 81a4a42e..892c079c 100644
+index 81a4a42..892c079 100644
 --- a/fdbcli/local.mk
 +++ b/fdbcli/local.mk
 @@ -22,14 +22,13 @@
@@ -75,7 +75,7 @@ index 81a4a42e..892c079c 100644
    fdbcli_LDFLAGS += -lc++
  endif
 diff --git a/fdbserver/local.mk b/fdbserver/local.mk
-index 78cad1bf..36f2c0f7 100644
+index 78cad1b..36f2c0f 100644
 --- a/fdbserver/local.mk
 +++ b/fdbserver/local.mk
 @@ -25,8 +25,7 @@ fdbserver_LDFLAGS := $(fdbrpc_LDFLAGS)

commit ad39735aedcbb8ef8dfc92e3fb8e09556eb02354

foundationdb: Fix evaluation
Broken in 1fa2503f9dba814eb23726a25642d2180ce791c3

pkgs/top-level/all-packages.nix
@@ -2421,7 +2421,7 @@ with pkgs;
   fluentd = callPackage ../tools/misc/fluentd { };
 
   flvstreamer = callPackage ../tools/networking/flvstreamer { };
-  
+
   hmetis = callPackage_i686 ../applications/science/math/hmetis { };
 
   libbsd = callPackage ../development/libraries/libbsd { };
@@ -2531,7 +2531,7 @@ with pkgs;
     foundationdb52
     foundationdb60;
 
-  foundationdb = callPackage ../servers/foundationdb { stdenv = overrideCC stdenv gcc49; };
+  foundationdb = callPackage ../servers/foundationdb { stdenv49 = overrideCC stdenv gcc49; };
 
   fuse-7z-ng = callPackage ../tools/filesystems/fuse-7z-ng { };
 
@@ -3722,7 +3722,7 @@ with pkgs;
   lzip = callPackage ../tools/compression/lzip { };
 
   luxcorerender = callPackage ../tools/graphics/luxcorerender { };
-  
+
   xz = callPackage ../tools/compression/xz { };
 
   lz4 = callPackage ../tools/compression/lz4 { };
@@ -4987,7 +4987,7 @@ with pkgs;
   securefs = callPackage ../tools/filesystems/securefs { };
 
   seexpr = callPackage ../development/compilers/seexpr { };
- 
+
   setroot = callPackage  ../tools/X11/setroot { };
 
   setserial = callPackage ../tools/system/setserial { };