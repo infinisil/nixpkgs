6d767ae68d1b55e9d0f53e93f85e5b581068c9be
Silvan Mosberger <contact@infinisil.com>
2020-03-12 15:23:38 +0100

    lib/modules: Remove internal _module attribute from result

--- lib/modules.nix
+++ lib/modules.nix
@@ -93,7 +93,10 @@ rec {
             res set._definedNames
         else
           res;
-      result = { inherit options config; };
+      result = {
+        inherit options;
+        config = removeAttrs config [ "_module" ];
+      };
     in result;
 
   # collectModules :: (modulesPath: String) -> (modules: [ Module ]) -> (args: Attrs) -> [ Module ]
